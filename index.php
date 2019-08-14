48b3e10caba6f0029970f38820317ebdec87f21b
getupmen14 <53499681+getupmen14@users.noreply.github.com>
2019-08-14 09:59:46 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head><title>Login Page</title></head>
+<body align="center">
+<form action="/action_page.html">
+	<h1>Login</h1>
+	<div class="container">
+		<label for="uname"><b>Username:</b></label>
+		<input type="text" placeholder="Enter Username"
+	<name="uname" required>
+	<br/>
+	<br/>
+
+		<label for="psw"><b>Password:</b></label>
+		<input type="text" placeholder="Enter Password"
+	<name="psw" required>
+	<br/>
+	<br/>
+
+	<button type="submit">Sign In</button>
+	<br/>
+	<br/>
+	<button type="submit">Reset Password</button>
+	<a href="resetpassword.html">
+	<br/>
+	<br/>
+	<button type="submit">Sign Up</button>
+</form>
+</body>
